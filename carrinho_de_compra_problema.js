643381050ae2b127d297c7814b393d6081791a2f
silvamicael <micael_s_silva@estudante.sesisenai.org.br>
2026-02-25 21:43:36 -0300

    Add Exemplo de carrinho com espoco público

---
+++ carrinho_de_compra_problema.js
@@ -0,0 +1,35 @@
+// Tudo exposto no escopo global
+
+let itens = [];
+let total = 0;
+
+function adicionar(item) {
+  itens.push(item);
+  calcularTotal();
+}
+
+function remover(nome) {
+  itens = itens.filter(item => item.nome !== nome);
+  calcularTotal();
+}
+
+function calcularTotal() {
+  total = itens.reduce((soma, item) => soma + item.preco, 0);
+}
+
+function verTotal() {
+  return total;
+}
+
+function listarItens() {
+  return itens;
+}
+
+adicionar({ nome: "Camisa", preco: 50 });
+adicionar({ nome: "Tênis", preco: 200 });
+
+console.log(listarItens());
+console.log("Total:", verTotal());
+
+itens = "bagunça total";
+total = -9999;
